ff3f46ac460719736e1ff0ba3a55a90f02194301
marco <marcoramosj@MacBook-Air-de-Marco-2.local>
2025-04-28 08:47:57 -0600

    se corrigio la bienvenida

---
+++ src/app/bienvenida/page.tsx
@@ -0,0 +1,10 @@
+"use client"; 
+
+export default function BienvenidaPage() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>¡Bienvenido a GreenPark!</h1>
+      <p>Esperamos que disfrutes tu experiencia.</p>
+    </div>
+  );
+}
